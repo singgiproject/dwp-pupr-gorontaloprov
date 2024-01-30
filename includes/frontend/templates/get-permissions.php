46dc73c230d522afc1793cc3050464d33eb3c330
Singgi Mokodompit <singgimokodompit.it@gmail.com>
2024-01-31 03:28:10 +0800

    Fixing Bug

--- includes/frontend/templates/get-permissions.php
+++ includes/frontend/templates/get-permissions.php
@@ -4,7 +4,7 @@
     <section class="section dashboard">
       <div class="row">
         <div class="head-title-content mt-5 mb-3">
-          <h4><span>ABSENSI</span> KARYAWAN</h4>
+          <h4><span>ABSENSI</span> ANGGOTA</h4>
         </div>
         <h2>Untuk melanjutkan absensi harap izinkan lokasi anda</h2>
         <i class="bi bi-hand-index-thumb"></i>
